7150174fad7bd6d2c1891f3466f69cc788ae064f
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-11-24 11:42:54 +0000

    Update Task-3.1.6.js

---
+++ module-3/scripts/Task-3.1.6.js
@@ -0,0 +1,8 @@
+var strMealCost = 45.67;
+var strServiceChargePercent = 12.5;
+
+var strTotalBill = strMealCost * (strServiceChargePercent/100);
+
+document.write("Your total bill, including the service charge, is £"+strTotalBill);
+
+document.write("Your total bill, rounded to two decimal points, is £"+strTotalBill.toFixed(2));
