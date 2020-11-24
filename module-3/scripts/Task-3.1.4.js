dbe502ca538b4d6aa1db734cd2c572ec0e300e49
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-11-24 11:19:58 +0000

    Update Task-3.1.4.js

---
+++ module-3/scripts/Task-3.1.4.js
@@ -0,0 +1,13 @@
+var myAge = 35;
+
+var rustysAge = 2;
+
+var totalAge = myAge + rustysAge;
+
+
+
+document.write(totalAge);
+alert (myAge / rustysAge);
+console.log (myAge - rustysAge);
+document.write(myAge % 2);
+
