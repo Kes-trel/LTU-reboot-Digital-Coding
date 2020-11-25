5a904f1e5f3f843b1a917b30af875eed1629f846
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-11-25 13:50:08 +0000

    Update Task-3.2.6.js

---
+++ module-3/scripts/Task-3.2.6.js
@@ -0,0 +1,11 @@
+var temperature = 30;
+
+if (temperature < 0 ){
+    console.log("Stay inside!")
+} else if (temperature < 30) {
+    console.log("Wear your hat and cote!")
+} else if (temperature < 50) {
+    console.log("Wear a coat!")
+} else {
+    console.log("Comando time!")
+}
