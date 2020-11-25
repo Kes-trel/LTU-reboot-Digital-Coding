ca3868ddf76425af6da6453121fe89008f16f275
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-11-25 11:29:12 +0000

    Create Boolean

---
+++ module-3/scripts/boolean.js
@@ -0,0 +1,11 @@
+var myName = "";
+var numOfKinds = 0;
+var isMarried;
+
+var myName1 = "Rusty"
+
+console.log("myName is: " + Boolean(myName));
+console.log("numOfKinds: " + Boolean(numOfKinds));
+console.log("isMarried: " + Boolean(isMarried));
+
+console.log("myName is: " + Boolean(myName1));
