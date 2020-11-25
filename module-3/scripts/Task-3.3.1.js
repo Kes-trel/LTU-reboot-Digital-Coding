aba81859d48e179525ee2ad24d915b72c0dbccb3
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-11-25 14:11:28 +0000

    Update Task-3.3.1.js

--- module-3/scripts/Task-3.3.1.js
+++ module-3/scripts/Task-3.3.1.js
@@ -1,3 +1,7 @@
-function percentageCalculator {
-    var number
+function percentageCalculator(number, percentage) {
+    var result = number * (percentage / 100);
+    return result;
 }
+
+console.log(percentageCalculator(135,30))
+console.log(percentageCalculator(207,12.5).toFixed(2))
