f5cf4d66d7e8a3a6a0cf9a8b6d3c309cbb8c1066
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-11-26 10:51:26 +0000

    Update Task-3.4.3.js

---
+++ module-3/scripts/Task-3.4.3.js
@@ -0,0 +1,5 @@
+var myFavouriteFoods = ["pizza", "ice-cream", "steak", "chocolate", "orange"];
+
+for (var index = 0; index <= myFavouriteFoods.length; index++) {
+    document.write("<p>" + myFavouriteFoods[index] + "</p>")
+}
