ca8db04688f4d90ef72a7118f2eea13c0fd9ef38
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-11-26 13:57:57 +0000

    Update Task-3.5.1.js

--- module-3/scripts/Task-3.5.1.js
+++ module-3/scripts/Task-3.5.1.js
@@ -1,65 +1,65 @@
 let shoppingCart = [
-    {​​
+    {
     name:"loaf of bread",
     type:"food",
     quantity:1,
     price:.85
-    }​​,
-    {​​
+  },
+  { 
     name:"multipack beans",
     type:"food",
     quantity:1,
     price:1
-    }​​,
-    {​​
-    name:"mushrooms",
-    type:"food",
-    quantity:10,
-    price:.1
-    }​​,
-    {​​
-    name:"can of beer",
-    type:"alcohol",
-    quantity:4,
-    price:1.1
-    }​​,
-    {​​
-    name:"prosecco",
-    type:"alcohol",
-    quantity:1,
-    price:8.99
-    }​​,
-    {​​
-    name:"steak",
-    type:"food",
-    quantity:2,
-    price:3.99
-    }​​,
-    {​​
-    name:"blue cheese",
-    type:"food",
-    quantity:1,
-    price:2.99
-    }​​,
-    {​​
-    name:"candles",
-    type:"home",
-    quantity:3,
-    price:1.99
-    }​​,
-    {​​
-    name:"cheesecake",
-    type:"food",
-    quantity:1,
-    price:4.99
-    }​​,
-    {​​
-    name:"onions",
-    type:"food",
-    quantity:3,
-    price:.4
-    }​​
-];
+},
+{ 
+  name:"mushrooms",
+  type:"food",
+  quantity:10,
+  price:.1
+},
+{
+  name:"can of beer",
+  type:"alcohol",
+  quantity:4,
+  price:1.1
+},
+{
+  name:"prosecco",
+  type:"alcohol",
+  quantity:1,
+  price:8.99
+},
+{
+  name:"steak",
+  type:"food",
+  quantity:2,
+  price:3.99
+},
+{
+  name:"blue cheese",
+  type:"food",
+  quantity:1,
+  price:2.99
+},
+{
+  name:"candles",
+  type:"home",
+  quantity:3,
+  price:1.99
+},
+{ 
+  name:"cheesecake",
+  type:"food",
+  quantity:1,
+  price:4.99
+},
+{ 
+  name:"onions",
+  type:"food",
+  quantity:3,
+  price:.4
+}];
+
 
     function shoppingCartTotalPrice( arr ) { //1. create a function that takes one parameter (the Array)
 
@@ -72,8 +72,10 @@ let shoppingCart = [
 
     //4. Return the totalPrice variable
 
-    return totalPrice;
+    return totalPrice.toFixed(2);
 
     }
 
-    shoppingCartTotalPrice(shoppingCart);
+    let message = "The cost of all your items comes to: £";
+
+    console.log(message + shoppingCartTotalPrice(shoppingCart));
