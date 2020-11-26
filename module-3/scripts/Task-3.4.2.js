50568d72b3886d6d24b4a283ccff0a675d585cdf
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-11-26 10:40:07 +0000

    Update Task-3.4.2.js

--- module-3/scripts/Task-3.4.2.js
+++ module-3/scripts/Task-3.4.2.js
@@ -1,1 +1,5 @@
-var myFavouriteFoods = ["pizza", "ice-cream", "steak", "chocolate"]
+var myFavouriteFoods = ["pizza", "ice-cream", "steak", "chocolate", "orange"];
+
+for (var index = 0; index <= myFavouriteFoods.length; index++) {
+    console.log(myFavouriteFoods[index])
+}
