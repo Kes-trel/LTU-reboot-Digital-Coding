a051e22983c709c90a3d11b7f35ee2b31903cb74
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-11-27 11:16:40 +0000

    Update Task-3.6.2.js

--- module-3/scripts/Task-3.6.2.js
+++ module-3/scripts/Task-3.6.2.js
@@ -2,4 +2,8 @@ let div = document.querySelector("div");
 
 let paragraph = document.createElement("p");
 
-let paragrapText = document.createTextNode("I am a paragraph created in JavaScript");
+let paragraphText = document.createTextNode("I am a paragraph created in JavaScript");
+
+    paragraph.appendChild( paragraphText);
+
+div.appendChild(paragraph)
