640adcd4f9047fc68555c791994835e342da7f52
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-11-27 14:10:24 +0000

    Update Task-3.6.5.js

--- module-3/scripts/Task-3.6.5.js
+++ module-3/scripts/Task-3.6.5.js
@@ -1,7 +1,12 @@
+let div = document.querySelector("div");
+
 let input = document.querySelector("input");
 
 let button = document.querySelector("button");
 
-    button.addEventListener("click", function(){
-        document.write(input.value)
+    button.addEventListener("click", function(event){
+        
+        event.preventDefault();
+
+        div.textContent = "Hello " + input.value;
     });
