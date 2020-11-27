dfb2104414c92ff56de4498867ba16a1c477903a
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-11-27 11:38:31 +0000

    Update Task-3.6.3.js

--- module-3/scripts/Task-3.6.3.js
+++ module-3/scripts/Task-3.6.3.js
@@ -1,5 +1,11 @@
 var button = document.getElementById("myBtn");
 
-button.addEventListener("mouseover", function (event){
+// button.addEventListener("mouseover", function (event){
+//     alert("Hi!");
+// })
+
+
+var sayHi = function (event) {
     alert("Hi!");
-})
+}
+button.addEventListener("mouseover",sayHi);
