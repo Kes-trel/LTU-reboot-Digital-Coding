8797c288e3ff9a751a0982dbdb580fe1cb90b0bd
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-11-27 13:38:37 +0000

    Add link to html and JS preventDefault

---
+++ module-3/scripts/Task-3.6.4.js
@@ -0,0 +1,6 @@
+let link = document.querySelector("a");
+
+link.addEventListener("click", function(event) {
+    event.preventDefault();
+    event.addEventListener(alert("Link is not working"))
+});
