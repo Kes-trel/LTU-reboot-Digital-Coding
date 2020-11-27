71fc2d6a1116f92a95fce930479c05b643f88241
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-11-27 10:41:15 +0000

    Update DOM

--- module-3/scripts/DOM.js
+++ module-3/scripts/DOM.js
@@ -11,3 +11,9 @@ let unlistitems = document.querySelectorAll("li");
 console.log(unlistitems[1]);
 
 let itemClass = document.getElementsByClassName("item");
+
+let image = document.getElementById("image");
+
+console.log(image.src);
+
+image.setAttribute("src", "https://images.unsplash.com/photo-1606318005254-bdb2bcd14d34?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHw%3D&auto=format&fit=crop&w=700&q=60")
