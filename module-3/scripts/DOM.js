4c4cc7ddef4d63f78bcf51d7b91bd36d21f32576
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-11-27 10:19:58 +0000

    query select

---
+++ module-3/scripts/DOM.js
@@ -0,0 +1,13 @@
+let heading1 = document.querySelector("h1");
+
+console.log(heading1);
+
+let paragraph = document.querySelector("p");
+
+console.log(paragraph);
+
+let unlistitems = document.querySelectorAll("li");
+
+console.log(unlistitems[1]);
+
+let itemClass = document.getElementsByClassName("item");
