0a2d53f6ba47e17349c92c1d1178a7766bd152cd
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-11-27 11:09:00 +0000

    Update Task-3.6.1.js

---
+++ module-3/scripts/Task-3.6.1.js
@@ -0,0 +1,13 @@
+var wholePage = document.querySelector("h1");
+
+wholePage.style.fontFamily = "Impact, Charcoal, sans-serif";
+wholePage.style.backgroundColor = "yellow";
+
+var image = document.querySelector("img");
+
+image.setAttribute("src", "https://images.unsplash.com/photo-1606312321098-190837c01672?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHw%3D&auto=format&fit=crop&w=700&q=60");
+
+var linkChange = document.querySelector("a")
+
+linkChange.setAttribute("href", "https://www.skynews.com")
+
