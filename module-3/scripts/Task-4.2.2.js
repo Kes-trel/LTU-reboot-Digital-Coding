056cad106d32683acea7f5d7f5bdc3e01f5cfd3e
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-12-01 11:34:43 +0000

    Add Reading list JS to the task

---
+++ module-3/scripts/Task-4.2.2.js
@@ -0,0 +1,27 @@
+
+/*
+FIXED START
+        function called "FixStart"
+        function has a single variable assigned to it
+        function checks first letter
+        if the first letter repeats itself later in the variable - word
+        then it replaces the the consecutive letter with a "*"
+        but not the first letter itself
+
+        FUNCTION ("Fixed Start")
+        VAR = WORD
+        FOR LOOP:
+        set a counter to first letter of the string [0]
+        condition - break when counter is equal to length of the string
+        increment counter by 1 letter [1++] (not quite sure the code for this yet)
+
+        IF letter is equal to "first letter" then
+            print "*" instead
+        ELSE just print a letter (i assume it will print in seperate lines - need to try)
+
+        RETURN result
+
+        Print results
+
+*/
+
